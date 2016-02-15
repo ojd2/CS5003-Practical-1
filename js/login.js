0dd9571e7465d2bac02a39e17f5c107416d82983
ojd2 <ojd2@st-andrews.ac.uk>
2016-02-15 14:15:52 +0000

    update

--- js/login.js
+++ js/login.js
@@ -8,11 +8,11 @@ var helper = (function() {
      *   other authentication information.
      */
     onSignInCallback: function(authResult) {
-      $('#authResult').html('Auth Result:<br/>');
-      for (var field in authResult) {
-        $('#authResult').append(' ' + field + ': ' +
-            authResult[field] + '<br/>');
-      }
+      // $('#authResult').html('Auth Result:<br/>');
+      // for (var field in authResult) {
+      //   $('#authResult').append(' ' + field + ': ' +
+      //       authResult[field] + '<br/>');
+      // }
 
       // Show hidden area of application once signed in.
       if (authResult.isSignedIn.get()) {
@@ -45,56 +45,7 @@ var helper = (function() {
       // Revoke the access token.
       auth2.disconnect();
     },
-    /**
-     * Gets and renders the list of people visible to this app.
-     */
-    // people: function() {
-    //   gapi.client.plus.people.list({
-    //     'userId': 'me',
-    //     'collection': 'visible'
-    //   }).then(function(res) {
-    //     var people = res.result;
-    //     $('#visiblePeople').empty();
-    //     $('#visiblePeople').append('Number of people visible to this app: ' +
-    //         people.totalItems + '<br/>');
-    //     for (var personIndex in people.items) {
-    //       person = people.items[personIndex];
-    //       $('#visiblePeople').append('<img src="' + person.image.url + '">');
-    //     }
-    //   });
-    // },
-    /**
-     * Gets and renders the currently signed in user's profile data.
-     */
-    // profile: function(){
-    //   gapi.client.plus.people.get({
-    //     'userId': 'me'
-    //   }).then(function(res) {
-    //     var profile = res.result;
-    //     console.log(profile);
-    //     $('#profile').empty();
-    //     $('#profile').append(
-    //         $('<p><img src=\"' + profile.image.url + '\"></p>'));
-    //     $('#profile').append(
-    //         $('<p>Hello ' + profile.displayName + '!<br />Tagline: ' +
-    //         profile.tagline + '<br />About: ' + profile.aboutMe + '</p>'));
-    //     if (profile.emails) {
-    //       $('#profile').append('<br/>Emails: ');
-    //       for (var i=0; i < profile.emails.length; i++){
-    //         $('#profile').append(profile.emails[i].value).append(' ');
-    //       }
-    //       $('#profile').append('<br/>');
-    //     }
-    //     if (profile.cover && profile.coverPhoto) {
-    //       $('#profile').append(
-    //           $('<p><img src=\"' + profile.cover.coverPhoto.url + '\"></p>'));
-    //     }
-    //   }, function(err) {
-    //     var error = err.result;
-    //     $('#profile').empty();
-    //     $('#profile').append(error.message);
-    //   });
-    // }
+    
   };// End return
 })();
 /**
